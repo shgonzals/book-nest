7a25556375c98d3545a962b1981ac8fb6b73de2a
shgonzals <sh.gonzal.s@gmail.com>
2023-11-04 22:51:39 +0100

    initial commit

---
+++ src/main/java/com/shgonzals/booknest/BookNestApplication.java
@@ -0,0 +1,13 @@
+package com.shgonzals.booknest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNestApplication.class, args);
+	}
+
+}
